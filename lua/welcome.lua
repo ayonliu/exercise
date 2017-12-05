72cb4c3e9ed4897cbfe14e03169b8bbf960c0ef8
root <root@homestead>
2017-12-05 10:47:17 +0000

    add static files

--- lua/welcome.lua
+++ lua/welcome.lua
@@ -1,4 +1,8 @@
 # lua script
 
 local template = require "resty.template"
-template.render("view.html", { message = "Hello, World!" })
+template.render("view.html", {
+    title = "test",
+    message = "Hello, World!",
+    css  = '<script src="css/bootstrap.min.css"></script>'
+})
