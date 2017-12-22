e8cf4ae790a5ff551fcb434d61d1a2c2761d037d
William Liu <ayon.liu@gmail.com>
2017-12-22 08:10:43 +0000

    update conf and static files

--- lua/welcome.lua
+++ lua/welcome.lua
@@ -1,8 +1,16 @@
 # lua script
 
 local template = require "resty.template"
+local css = {
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">',
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">',
+    '<link rel="stylesheet" type="text/css" href="/css/main.css">'
+}
+local js = {'<script src="/js/vue.min.js" async=""></script>'}
 template.render("view.html", {
     title = "test",
-    message = "Hello, World!",
-    css  = '<script src="css/bootstrap.min.css"></script>'
+    message = "",
+    css  = table.concat(css),
+    js   = table.concat(js),
 })
+
